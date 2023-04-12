84f6163d8daa12ed078bc9abda7673c0b05ad62e
Ariana <ariaguirre64@gmail.com>
2023-04-12 18:27:01 -0300

    test

--- api/tests/routes.test.js
+++ api/tests/routes.test.js
@@ -1,34 +1,35 @@
 const request = require('supertest');
-// const app = require('../src/app')
 const app = require('../src/app.js');
 
-const pokemonsRouter = require("../src/routes/pokemonsRouter")
 
-// describe('GET /', () =>{
-//     it('should return a list of pokemons', async () => {
-//         const response = await request(pokemonsRouter).get('/') 
-//         expect(response.status).toBe(200);
-//             expect(response.body).toBeInstanceOf(Array)
-//         })
-//         // expect(response.body).toHaveProperty('id');
-//         // expect(response.body).toHaveProperty('name');
-//     });
-
-    describe("GET /", () => {
-        it("should return an array of pokemons", async () => {
-          const res = await request(pokemonsRouter).get("/");
-          expect(res.statusCode).toEqual(200);
-        //   expect(Array.isArray(res.body)).toBe(true);
-        });
-      });
+  describe("GET /", () => {
+    it("should return an array of pokemons", async () => {
+    const response = await request(app).get("/pokemons");
+    expect(response.statusCode).toEqual(200);
+    expect(Array.isArray(response.body)).toBe(true);
+  });
+});
 
 
-// describe('GET /', () =>{
-//     it('should return a list of pokemons', async () => {
-//         const response = await request(pokemonsRouter).get('/').then(response => {expect(response.status).toBe(200);
-//             // expect(response.body).toBeInstanceOf(Array)
-//         })
-//         // expect(response.body).toHaveProperty('id');
-//         // expect(response.body).toHaveProperty('name');
-//     });
-// });
+  describe("GET /id", () => {
+    it('should return an object', async () => {
+      const id = 1; 
+      const response = await request(app).get(`/pokemons/${id}`);
+      expect(response.statusCode).toEqual(200);
+      expect(response.body).toBeInstanceOf(Object);
+    })
+    it('should return a statusCode 404 when id does not exist', async () => {
+      const id = 'no-valid';
+      const response = await request(app).get(`/pokemons/${id}`);
+      expect(response.statusCode).toBe(404);
+    })
+  })
+
+    
+    // describe("POST /", () => {
+    //   it('should create a new Pokemon', async ()=> {
+    //     const response = await request(app).post("/pokemons");
+    //     expect(response.statusCode).toEqual(201);
+    //     expect(Array.isArray(response.body)).toBe(true);
+    //   })
+    // })
